27b544b1cfcb5456153ddc587a03edb0502df5ad
linux2311 <linus.bongen@outlook.de>
2024-05-28 12:56:02 +0200

    add irrigation-system

---
+++ irrigation-system.ino
@@ -0,0 +1,31 @@
+const int soilMoistPin = A5;
+const int soilMoistLevelLow = 600;      // your value
+const int soilMoistLevelHigh = 250;     // your value
+const int relayPin = 13;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(soilMoistPin, INPUT);
+  pinMode(relayPin, OUTPUT);
+}
+
+void loop() {
+  int soilMoist = analogRead(soilMoistPin);
+
+  Serial.print("Analog Value: ");
+  Serial.print(soilMoist);
+  int percentSoilMoist = map(soilMoist, soilMoistLevelLow, soilMoistLevelHigh, 0, 100);
+
+  Serial.print("\t");
+  Serial.print(percentSoilMoist);
+  Serial.println(" %");
+
+  percentSoilMoist = 40;        // clear this line 
+
+  if(percentSoilMoist < 30) {
+    digitalWrite(relayPin, HIGH);
+    delay(1*1000); 
+    digitalWrite(relayPin, LOW);
+  }
+  delay(5*1000);
+}
